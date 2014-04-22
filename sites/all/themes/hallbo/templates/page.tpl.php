84d4d6467c849782a45ac0aebd7d3d37ca190e4d
Johan Östling <kjostling@gmail.com>
2014-04-23 01:03:57 +0200

    first print css

--- sites/all/themes/hallbo/templates/page.tpl.php
+++ sites/all/themes/hallbo/templates/page.tpl.php
@@ -126,7 +126,7 @@
   <div class="row">
 
     <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-sm-3" role="complementary">
+      <aside class="sidebar-first col-sm-3" role="complementary">
         <?php print render($page['sidebar_first']); ?>
       </aside>  <!-- /#sidebar-first -->
     <?php endif; ?>
